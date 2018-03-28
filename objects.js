79135ee5e060178f72b0b4aaa2f73a7ea4a5ce80
Dominik Nettesheim <dominiknetteshein@students.berkeley.net>
2018-03-28 10:33:48 -0700

    Initial commit

---
+++ objects.js
@@ -0,0 +1,195 @@
+var artist = function(name,value){
+    this.name = name;
+    this.value = value;
+};
+
+var ES = new artist("Ed Sheeran","ed+sheeran");
+var D  = new artist("Drake","drake");
+var ID = new artist("Imagine Dragons","imagine+dragons");
+var PM = new artist("Post Malone","post+malone");
+var KL = new artist("Kendrick Lamar","kendrick+lamar");
+var BM = new artist("Bruno Mars","bruno+mars");
+var CS = new artist("Chris Stapleton","chris+stapleton");
+var CC = new artist("Camila Cabello","camila+cabello");
+var M  = new artist("Migos","migos");
+var CB = new artist("Cardi B","cardi+b");
+var JT = new artist("Justin Timberlake","justin+timberlake");
+var XT = new artist("XXXTentacion","xxxtentacion");
+var P  = new artist("P!nk","pink");
+var H  = new artist("Halsey","halsey");
+var TL = new artist("Tory Lanez","tory+lanez");
+var M5 = new artist("Maroon 5","maroon+5");
+var Bts= new artist("BTS","bts");
+var DL = new artist("Dua Lipa","dua+lipa");
+var Sza= new artist("SZA","sza");
+var Nf = new artist("NF","nf");
+var TR = new artist("Thomas Rhett","thomas+rhett");
+var K  = new artist("Khalid","khalid");
+var CP = new artist("Charlie Puth","charlie+puth");
+var DLo= new artist("Demi Lovato","demi+lovato");
+var Ma = new artist("Marshmello","marshmello");
+var LB = new artist("Luke Bryan","luke+bryan");
+var KB = new artist("Kane Brown","kane+brown");
+var TW = new artist("The Weeknd","the+weeknd");
+var FGL= new artist("Florida Georgia Line","florida+georgia+line");
+var E  = new artist("Eminem","eminem");
+var MT = new artist("Meghan Trainor","meghan+trainor");
+var Me = new artist("Metallica","metallica");
+var DJK= new artist("DJ Khaled","dj+khaled");
+var GE = new artist("G-Eazy","g-eazy");
+var TS = new artist("Taylor Swift","taylor+swift");
+var SN = new artist("6ix9ine","6ix9ine");
+var SM = new artist("Shawn Mendez","shawn+mendez");
+var JA = new artist("Jason Aldean","jason+aldean");
+var TN = new artist("Tech N9ine","tech+n9ine");
+var L  = new artist("Logic","logic");
+var PTM= new artist("Portugal. The Man","portugal.+the+man");
+var MJ = new artist("Micheal Jackson","micheal+jackson");
+var LC = new artist("Luke Combs","luke+combs");
+var R  = new artist("Rihanna","rihanna");
+var BR = new artist("Bebe Rexha","bebe+rexha");
+var SS = new artist("Sam Smith","sam+smith");
+var S21= new artist("21 Savage","21+savage");
+var NBA= new artist("YoungBoy Never Broke Again","youngboy+never+broke+again");
+var LUV= new artist("Lil Uzi Vert","lil+uzi+vert");
+var A  = new artist("Adele","adele");
+var BS = new artist("Blake Shelton","blake+shelton");
+var CBr= new artist("Chris Brown","chris+brown");
+var TC = new artist("The Chainsmokers","the+chainsmokers");
+var B  = new artist("Bazzi","bazzi");
+var O  = new artist("Ozuna","ozuna");
+var BBJ= new artist("BlocBoy Jb","blocboy+jb");
+var BY = new artist("Brett Young","brett+young");
+var TOP= new artist("Twenty One Pilots","twenty+one+pilots");
+var DY = new artist("Daddy Yankee","daddy+yankee");
+var Z  = new artist("Zedd","zedd");
+var SG = new artist("Selena Gomez","selena+gomez");
+var RTK= new artist("Rich The Kid","rich+the+kid");
+var TSc= new artist("Travis Scott","travis+scott");
+var F  = new artist("Future","future");
+var KBl= new artist("Kodak Black","kodak+black");
+var Max= new artist("MAX","max");
+var JBa= new artist("J Balvin","j+balvin");
+var CU = new artist("Carrie Underwood","carrie+underwood");
+var BJ = new artist("Bon Jovi","bon+jovi");
+var Ke = new artist("Kesha","kesha");
+var Exo= new artist("EXO","exo");
+var TB = new artist("The Beatles","the+beatles");
+var JBi= new artist("Justin Bieber","justin+bieber");
+var Bey= new artist("Beyonce","beyonce");
+var MM = new artist("Maren Morris","maren+morris");
+var Mac= new artist("Macklemore","macklemore");
+var NH = new artist("Niall Horan","niall+horan");
+var GM = new artist("Gucci Mane","gucci+mane");
+var OD = new artist("Old Dominion","old+dominion");
+var SMc= new artist("Scotty Mccreer","scotty+mccreer");
+var NM = new artist("Nicki Minaj","nicki+minaj");
+var AJ = new artist("Alan Jackson","alan+jackson");
+var MMe= new artist("MercyMe","mercyme");
+var YT = new artist("Young Thug","young+thug");
+var Pr = new artist("Prince","prince");
+var KC = new artist("Kelly Clarkson","kelly+clarkson");
+var G  = new artist("Grey","grey");
+var JD = new artist("Jordan Davis","jordan+davis");
+var BB = new artist("Bad Bunny","bad+bunny");
+var J  = new artist("Journey","journey");
+var JH = new artist("J-Hope","j-hope");
+var Mi = new artist("Miguel","miguel");
+var LyS= new artist("Lynyrdn Skynyrd","lynyrd+skynyrd");
+var MB = new artist("Metro Boomin","metro+boomin");
+var TDS= new artist("Ty Dolla $ign","ty+dolla+$ign");
+var LZ = new artist("Led Zeppelin","led+zeppelin");
+var TBr= new artist("The Breeders","the+breeders");
+var WTM= new artist("WALK THE MOON","walk+the+moon");
+var AC = new artist("Alessia Cara","alessia+cara");
+var DR = new artist("Darius Rucker","darius+rucker");
+
+
+var allArtists = [ES,D,ID,PM,KL,BM,CS,CC,M,CB,JT,XT,P,H,TL,M5,Bts,DL,Sza,Nf,TR,K,CP,DLo,Ma,LB,KB,TW,FGL,E,MT,Me,DJK,GE,TS,SN,SM,JA,TN,L,PTM,MJ,LC,R,BR,SS,S21,NBA,LUV,A,BS,CBr,TC,B,O,BBJ,BY,TOP,DY,Z,SG,RTK,TSc,F,KBl,Max,JBa,CU,BJ,Ke,Exo,TB,JBi,Bey,MM,Mac,NH,GM,OD,SMc,NM,AJ,MMe,YT,Pr,KC,G,JD,BB,J,JH,Mi,LyS,MB,TDS,LZ,TBr,WTM,AC,DR];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    $('#searchSongs').on('click',function(){
+        var artistURL = $('#selectArtist').val();
+        $.ajax({
+            url: "https://itunes.apple.com/search?term=" + artistURL,
+            type: 'GET',
+            crossDomain: true,
+            dataType: 'jsonp',
+            success: function(result) {
+                console.log(result);
+                myFunction(result);
+            },
+            error: function() { alert('Failed!'); }
+        });
+    });
+
+    function fillList(){
+        for (var i = 0; i < allArtists.length; i++){
+            document.getElementById("selectArtist").innerHTML += "<option value ='" + allArtists[i].value + "'>" + allArtists[i].name + "</option>";
+        }
+    }
+    fillList();
+
+});
+
+function myFunction(json){
+    var html = "";
+    var artist = $('#selectArtist').val();
+    if(artist == 0){
+        document.getElementById("songResults").innerHTML = "PLEASE SELECT AN ARTIST!";
+    }
+    else{
+        for(var s=0; s<$('#numberOfSongs').val(); s++){
+            html += "<tr><td><img src =" + json.results[s].artworkUrl60 + "></td>";
+            html += "<td>" + json.results[s].trackName + "</td>";
+            html += "<td>" + "<audio controls='true' src='" + json.results[s].previewUrl + "' id='audio' type='audio/m4a'></audio>" + "</td></tr>";
+        }
+        document.getElementById("songResults").innerHTML = html;
+    }
+
+
+}
